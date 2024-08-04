ea4c6dd09077c23258ba019d095fdd828868f609
szborok <112414585+szborok@users.noreply.github.com>
2024-08-04 23:19:08 +0200

    add files via upload

---
+++ zarovizsga_gyak_3/zarovizsga_gyak_3/src/main/java/be03/borok_szabolcs/App.java
@@ -0,0 +1,13 @@
+package be03.borok_szabolcs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
